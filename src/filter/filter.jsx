94dc62df735902c6a173599bff778a5c102213b3
Boburjon17 <boburjonsodiqov19@gmail.com>
2024-07-26 10:05:22 +0500

    filter

---
+++ src/filter/filter.jsx
@@ -0,0 +1,18 @@
+import "./filter.jsx"
+
+const Filter = () => {
+  return (
+    <div>
+        <div className="back">
+            <p>Главная</p>
+            <i class="fa-solid fa-chevron-right"></i>
+        </div>
+        <div className="title-filter">
+            <h1>Смартфоны в Ташкенте</h1>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Filter
